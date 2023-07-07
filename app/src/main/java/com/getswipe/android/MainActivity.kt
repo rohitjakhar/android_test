1cea35bb1d129945d475802e9a4aef2d7a15dd44
Rohit Jakhar <rohit@appflixstudio.com>
2023-07-07 09:14:31 +0530

    Add action bar and handing navigation.

--- app/src/main/java/com/getswipe/android/MainActivity.kt
+++ app/src/main/java/com/getswipe/android/MainActivity.kt
@@ -2,13 +2,25 @@ package com.getswipe.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.getswipe.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.containerMain.id) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
